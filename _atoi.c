b0d7f78311138d73ff86cc7e4e3089a7733129ca
Your Name <you@example.com>
2023-05-22 12:13:11 -0700

    interactive, is_delim, _isalpha, and _atoi

--- _atoi.c
+++ _atoi.c
@@ -1,36 +1,52 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
+ * interactive - returns true if shell is in interactive mode
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	int is_interactive = 0;
+
+	if (isatty(STDIN_FILENO) && info->readfd <= 2)
+		is_interactive = 1;
+
+	return (is_interactive);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
+ * is_delim - checks if character is a delimiter
+ * @c: the character to check
+ * @delim: the delimiter string
+ *
  * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
 {
-	while (*delim)
-		if (*delim++ == c)
-			return (1);
-	return (0);
+	int is_delimiter = 0;
+
+	while (*delim != '\0')
+	{
+		if (*delim == c)
+		{
+			is_delimiter = 1;
+			break;
+		}
+
+		delim++;
+	}
+
+	return (is_delimiter);
 }
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: The character to input
+ * @c: The character to check
+ *
  * Return: 1 if c is alphabetic, 0 otherwise
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -42,15 +58,17 @@ int _isalpha(int c)
 /**
  * _atoi - converts a string to an integer
  * @s: the string to be converted
+ *
  * Return: 0 if no numbers in string, converted number otherwise
  */
-
 int _atoi(char *s)
 {
-	int i, sign = 1, flag = 0, output;
+	int i = 0;
+	int sign = 1;
+	int flag = 0;
 	unsigned int result = 0;
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	while (s[i] != '\0' && flag != 2)
 	{
 		if (s[i] == '-')
 			sign *= -1;
@@ -63,13 +81,10 @@ int _atoi(char *s)
 		}
 		else if (flag == 1)
 			flag = 2;
-	}
 
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
+		i++;
+	}
+	int output = (sign == -1) ? -result : result;
 
 	return (output);
 }
-
